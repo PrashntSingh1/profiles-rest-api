e7ed193ad99cc2b40671f8f366d5667b59a8fa0f
Prashant <prashant11singh.ps@gmail.com>
2021-04-09 19:30:44 +0530

    Updated Models.py

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -6,10 +6,11 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, name, email, password = None):
+    def create_user(self, email, name, password = None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
+
         email = self.normalize_email(email)
         user = self.model(email = email, name = name)
 
@@ -19,7 +20,7 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, password):
-        user = self.create_user(email,name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
